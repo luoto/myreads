c4b5718aa0875e7d3292148ccb97a9c27285908c
Tony Luo <luo.tony37@gmail.com>
2017-11-14 12:44:29 -0800

    refactor: Changed to functional component.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,11 @@
-import React from 'react'
+import React from 'react';
 
-class Header extends React.Component {
-  render() {
-    
-  }
+function Header(props) {
+  return (
+    <div className="list-books-title">
+      <h1>MyReads</h1>
+    </div>
+  )
 }
+
+export default Header;

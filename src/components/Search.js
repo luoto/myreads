4520f78fad1ca81bf4417efb4eb001dcfb0706bd
Tony Luo <luo.tony37@gmail.com>
2017-11-14 12:47:54 -0800

    reactor: Refactored static portions to dynamic portions.

--- src/components/Search.js
+++ src/components/Search.js
@@ -5,24 +5,37 @@ class Search extends React.Component {
     return (
       <div className="search-books">
         <div className="search-books-bar">
-          <a className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</a>
+          <Link to="/" className="close-search">Close</Link>
           <div className="search-books-input-wrapper">
-            {/*
-              NOTES: The search from BooksAPI is limited to a particular set of search terms.
-              You can find these search terms here:
-              https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-
-              However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-              you don't find a specific author or title. Every search is limited by search terms.
-            */}
-            <input type="text" placeholder="Search by title or author"/>
-
+            <input type="text" value={this.state.value} onChange={this.search} placeholder="Search by title or author"/>
           </div>
         </div>
         <div className="search-books-results">
-          <ol className="books-grid"></ol>
+          <ol className="books-grid">
+            {
+              this.state.books.map((book) => {
+                const thumbnail = book.imageLinks ? book.imageLinks.thumbnail : null;
+                const props = {
+                  id: book.id,
+                  shelf: book.shelf,
+                  title: book.title,
+                  authors: book.authors,
+                  thumbnail,
+                  moveShelf: this.moveShelf
+                };
+
+                return (
+                  <li key={book.id}>
+                    <Book {...props} />
+                  </li>
+                )
+              })
+            }
+          </ol>
         </div>
       </div>
     )
   }
 }
+
+export default Search;

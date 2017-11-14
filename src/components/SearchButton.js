1bb897eedd93c3de523e742cc55c56dcbe0aac71
Tony Luo <luo.tony37@gmail.com>
2017-11-14 12:45:33 -0800

    refactor: Changed to function component and introduced react-router-dom.

--- src/components/SearchButton.js
+++ src/components/SearchButton.js
@@ -1,11 +1,12 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
-class SearchButton extends React.Component {
-  render() {
-    return (
-      <div className="open-search">
-        <a onClick={() => this.setState({ showSearchPage: true })}>Add a book</a>
-      </div>
-    )
-  }
+function SearchButton(props) {
+  return (
+    <div className="open-search">
+      <Link to="/search">Add a book</Link>
+    </div>
+  )
 }
+
+export default SearchButton;

317faec756eb299291c0a1c34a9d241b8958e2e5
Tony Luo <luo.tony37@gmail.com>
2017-11-14 12:39:30 -0800

    feat: Put books in corresponding shelves.

--- src/components/BookShelf.js
+++ src/components/BookShelf.js
@@ -11,14 +11,34 @@ class BookShelf extends React.Component {
   componentDidMount() {
     BooksAPI.getAll().then((books) => this.setState({ books: books }));
   }
+
+  moveShelf = (id, newShelf, currentShelf) => {
+    BooksAPI.update({id}, newShelf).then((books) => {
+      this.setState(prevState => {
+        return prevState.books.map((book) => {
+          if (book.id === id) book.shelf = newShelf;
+          return book;
+        })
+      })
+    })
+  }
+
   render() {
+    const books = this.state.books;
+
     return (
+      <div>
       <div className="list-books">
+        <Header />
         <div className="list-books-content">
           <div>
+            <Shelf id="currentlyReading" shelfTitle="Currently Reading" books={books.filter(book => book.shelf === 'currentlyReading')} moveShelf={this.moveShelf}/>
+            <Shelf id="wantToRead" shelfTitle="Want to Read" books={books.filter(book => book.shelf === 'wantToRead')} moveShelf={this.moveShelf}/>
+            <Shelf id="read" shelfTitle="Read" books={books.filter(book => book.shelf === 'read')} moveShelf={this.moveShelf}/>
           </div>
         </div>
       </div>
+    </div>
     )
   }
 }

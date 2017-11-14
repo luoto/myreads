36ec3895d0de52f3291673757161f1bac175b4ec
Tony Luo <luo.tony37@gmail.com>
2017-11-14 12:27:14 -0800

    feat: Implement react router navigation

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
-import React from 'react'
-import * as BooksAPI from './BooksAPI'
-import './App.css'
+import React from 'react';
+import {BrowserRouter as Router, Route, Switch, Redirect} from 'react-router-dom';
+import './App.css';
+import Search from './components/Search';
+import BookShelf from './components/BookShelf';
 
 class BooksApp extends React.Component {
   render() {
     return (
-      <div className="app">
-
-      </div>
+      <Router>
+        <div className="app">
+          <Switch>
+            <Route exact path="/" component={BookShelf} />
+            <Route path="/search" component={Search} />
+            <Redirect to="/" />
+          </Switch>
+        </div>
+      </Router>
     )
   }
 }
 
-export default BooksApp
+export default BooksApp;
